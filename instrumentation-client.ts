0af226cb2c6e3ebdad040d31f80af79ab9d49676
Pamelin Nkomo <pamelinnkomo4@gmail.com>
2025-08-07 06:44:03 +0000

    TypeError: Failed to fetch     at method (https://6000-firebase-studio-1

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -3,7 +3,8 @@ import posthog from 'posthog-js'
 
 if (typeof window !== 'undefined') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    api_host: '/ingest',
+    ui_host: 'https://us.i.posthog.com',
     capture_pageview: false // pageviews are captured manually in PostHogProvider
   });
 }
