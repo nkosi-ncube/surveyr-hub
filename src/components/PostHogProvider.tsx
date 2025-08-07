cc0ae122beb4ad2326f3092e8decde4743149ddd
Pamelin Nkomo <pamelinnkomo4@gmail.com>
2025-08-07 06:00:39 +0000

    am not getting an events

--- src/components/PostHogProvider.tsx
+++ src/components/PostHogProvider.tsx
@@ -4,13 +4,6 @@ import { PostHogProvider as PHProvider } from 'posthog-js/react'
 import posthog from 'posthog-js'
 import { useEffect } from 'react'
 
-if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    capture_pageview: false // Disable automatic pageview capture, as we capture manually
-  })
-}
-
 export function PostHogPageview(): JSX.Element {
   const pathname = usePathname()
   const searchParams = useSearchParams()
